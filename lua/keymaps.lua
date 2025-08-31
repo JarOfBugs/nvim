2ed827e2ce681a99e5417a7a2f204fff81695055
Robbe <220320027+Yevonita@users.noreply.github.com>
2025-09-01 00:32:50 +0200

    feat: Add remap for saving and exploring directory

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,6 +7,12 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Save
+vim.keymap.set("n", "<leader>w", "<cmd>write<CR>", { desc = "[W]rite Current File" })
+
+-- File navigation
+vim.keymap.set("n", "<leader>e", "<cmd>Ex<CR>", { desc = "[E]xplore current directory" })
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
